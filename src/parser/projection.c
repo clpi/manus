c1bded9b3fec9e2a2c52f15f374c2641cca1de12
Chris P <chris@pecunies.com>
2026-09-02 15:50:23 -0700

    parser: transfer compatibility long text primary face

--- src/parser/projection.c
+++ src/parser/projection.c
@@ -3226,6 +3226,8 @@ __attribute__((visibility("default"))) int64_t event(int64_t fact[], int64_t cou
                         delimiter = 6;
         } else if ((kind == INT64_C(106))) {
                         delimiter = 7;
+        } else if ((kind == INT64_C(108))) {
+                        delimiter = 8;
         } else if ((kind == INT64_C(58))) {
             int64_t depth = 1;
             int64_t probe = (index + 1);
